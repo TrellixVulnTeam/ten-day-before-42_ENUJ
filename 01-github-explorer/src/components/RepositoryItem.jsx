1c2d687c6dd6adb3b9b76da57d76aec589cdec80
itallosavieira <itallo.sa.vieira@gmail.com>
2021-05-08 11:53:22 -0300

    Repositories App finished

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li>
-      <strong>{props.repository?.name ?? 'Default'}</strong>
-      <p>{props.repository?.description ?? 'React default'}</p>
-      <a target='_blank' href={props.repository?.link ?? 'https://github.com/itallosavieira'}>access repository</a>
-    </li>
-  )
-}
